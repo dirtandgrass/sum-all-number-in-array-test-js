eb86254f3478bb0dbe28fb2e918a91bf4c415f53
Luke <verdant.luke@gmail.com>
2024-02-04 20:12:19 -0500

    impl nested array sum

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  let sum = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) { // recursive case
+      sum += sumItems(item);
+    } else {
+      sum += item; // base case
+    }
+  }
+  return sum; // return the sum
+};
 
 module.exports = sumItems;
