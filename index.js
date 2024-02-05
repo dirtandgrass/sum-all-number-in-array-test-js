6fa8f13829d62a2509ab86ce367daf2d63aef3c1
Luke <verdant.luke@gmail.com>
2024-02-04 20:14:32 -0500

    added some error handling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const sumItems = (array) => {
+
+  if (!Array.isArray(array)) throw new Error('Input must be an array');
+
+
   let sum = 0;
   for (let item of array) {
     if (Array.isArray(item)) { // recursive case
       sum += sumItems(item);
-    } else {
+    } else if (typeof item === 'number') {
       sum += item; // base case
     }
   }
